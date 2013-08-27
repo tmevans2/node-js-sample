1123f521741bd9d29b8410e888ccea2eaf65002e
Ubuntu <ubuntu@ip-172-31-17-3.us-west-2.compute.internal>
2013-08-27 04:30:37 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,10 +3,10 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hello World2!');
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
